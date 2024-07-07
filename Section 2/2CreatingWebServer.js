1e1ebe582918deb433aae69f2f87209dca279c9f
sai.rane <sai.rane729@gmail.com>
2024-07-08 01:12:56 +0530

    section 2 half

--- Section 2/2CreatingWebServer.js
+++ Section 2/2CreatingWebServer.js
@@ -3,9 +3,11 @@ const http = require("http"); //importing http module. This will help us in buil
 // In order to build a server, we have to do 2 things: 1st we create a server and 2nd we start the server
 const server = http.createServer((req, res) => {
   //this callback function will get executed each time a new request hits a server
-  res.end("hello from the server side");
+  res.end("hello from the server side"); //With help of res we will send response to the client
 });
 
+//Now lets listens to the incoming requests from the client
+//The listen method accepts a couple of parameters as shown below. 1st is the port number, 2nd is host(means the current computer the program is currently running at, Standard IP address for localhost is 127.0.0.1), 3rd is the callbackfunction
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
 });
--- Section 2/3Routing.js
+++ Section 2/3Routing.js
@@ -1,10 +1,12 @@
+// Routing means implementing different url
+
 const http = require("http"); //importing http module. This will help us in building http server
 const url = require("url");
 
 // In order to build a server, we have to do 2 things: 1st we create a server and 2nd we start the server
 const server = http.createServer((req, res) => {
   //this callback function will get executed each time a new request hits a server
-  console.log(req.url);
+  console.log("req.url ->", req.url);
 
   const pathName = req.url;
   if (pathName == "/" || pathName == "/overview") {
